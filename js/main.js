c88cbf1e654951c821f60178c224bc95419bbdea
cyro14 <51408962+cyro14@users.noreply.github.com>
2026-09-14 14:06:31 -0400

    Add additional UI functions to global scope

--- js/main.js
+++ js/main.js
@@ -3,13 +3,21 @@ import { openCardGenerator, downloadCard } from './card.js';
 import { 
     render, checkWelcome, closeWelcome, closeModal, triggerToast, 
     changeTheme, toggleCompact, saveStateForUndo, undoAction, switchTab, 
-    filterGames, setPlatformFilter, togglePortableFilter, saveGame,
-    // (Importe aqui o restante das suas funções exportadas do ui.js)
+    filterGames, setPlatformFilter, togglePortableFilter, setHomeSort,
+    setFinishedSort, toggleCollapse, renderFinishedTab, saveGame,
+    openCatModal, openEditCatModal, saveCategory, askDeleteCategory, moveCategory,
+    openGameModal, openEditGameModal, askDeleteGame, toggleState,
+    openRatingModal, openEditFinishedModal, cancelRating, skipRating, saveRating, saveFinishedEdit,
+    spinRoulette, acceptRoulette, quickSearch, openImageModal, searchCoverOnGoogle, previewImageEdit, saveEditedImage,
+    openBackupModal, exportBackup, importBackup, openSettings, openFactoryReset, confirmFactoryReset,
+    renderPlatformAdmin, previewPlatformIcon, searchPlatformIconOnGoogle, editPlatform, cancelEditPlatform, savePlatform, removePlatform
 } from './ui.js';
 
-// Anexando ao escopo global (O Truque do Window)
+// Anexando ao escopo global para que o index.html possa ler os onlicks
 window.openCardGenerator = openCardGenerator;
 window.downloadCard = () => downloadCard(triggerToast);
+
+// Mapeamento massivo da UI
 window.checkWelcome = checkWelcome;
 window.closeWelcome = closeWelcome;
 window.closeModal = closeModal;
@@ -20,9 +28,44 @@ window.switchTab = switchTab;
 window.filterGames = filterGames;
 window.setPlatformFilter = setPlatformFilter;
 window.togglePortableFilter = togglePortableFilter;
+window.setHomeSort = setHomeSort;
+window.setFinishedSort = setFinishedSort;
+window.toggleCollapse = toggleCollapse;
 window.saveGame = saveGame;
-
-// (Continue pendurando todas as funções de clique da UI que você importou, ex: window.saveCategory = saveCategory)
+window.openCatModal = openCatModal;
+window.openEditCatModal = openEditCatModal;
+window.saveCategory = saveCategory;
+window.askDeleteCategory = askDeleteCategory;
+window.moveCategory = moveCategory;
+window.openGameModal = openGameModal;
+window.openEditGameModal = openEditGameModal;
+window.askDeleteGame = askDeleteGame;
+window.toggleState = toggleState;
+window.openRatingModal = openRatingModal;
+window.openEditFinishedModal = openEditFinishedModal;
+window.cancelRating = cancelRating;
+window.skipRating = skipRating;
+window.saveRating = saveRating;
+window.saveFinishedEdit = saveFinishedEdit;
+window.spinRoulette = spinRoulette;
+window.acceptRoulette = acceptRoulette;
+window.quickSearch = quickSearch;
+window.openImageModal = openImageModal;
+window.searchCoverOnGoogle = searchCoverOnGoogle;
+window.previewImageEdit = previewImageEdit;
+window.saveEditedImage = saveEditedImage;
+window.openBackupModal = openBackupModal;
+window.exportBackup = exportBackup;
+window.importBackup = importBackup;
+window.openSettings = openSettings;
+window.openFactoryReset = openFactoryReset;
+window.confirmFactoryReset = confirmFactoryReset;
+window.previewPlatformIcon = previewPlatformIcon;
+window.searchPlatformIconOnGoogle = searchPlatformIconOnGoogle;
+window.editPlatform = editPlatform;
+window.cancelEditPlatform = cancelEditPlatform;
+window.savePlatform = savePlatform;
+window.removePlatform = removePlatform;
 
 // Inicialização Principal
 document.addEventListener("DOMContentLoaded", () => {
